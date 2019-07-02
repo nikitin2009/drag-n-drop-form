6df286da444f7624ae90f8defadec7f7cb80ff9b
Alex N <nikitin2009@gmail.com>
2019-07-02 10:54:31 +0300

    Updates

---
+++ precache-manifest.1eebd72c83a34db8a265116869fe772d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e871793826202af9958396fea4b0b0",
+    "url": "/drag-n-drop-form/index.html"
+  },
+  {
+    "revision": "4e0b185993292983d539",
+    "url": "/drag-n-drop-form/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "eb113a6cc9f14f1ac268",
+    "url": "/drag-n-drop-form/static/css/main.4ea25b19.chunk.css"
+  },
+  {
+    "revision": "4e0b185993292983d539",
+    "url": "/drag-n-drop-form/static/js/2.61a831b9.chunk.js"
+  },
+  {
+    "revision": "eb113a6cc9f14f1ac268",
+    "url": "/drag-n-drop-form/static/js/main.1aca6b62.chunk.js"
+  },
+  {
+    "revision": "0c5894bba67a27bd73ab",
+    "url": "/drag-n-drop-form/static/js/runtime~main.d71e8d80.js"
+  }
+]);
